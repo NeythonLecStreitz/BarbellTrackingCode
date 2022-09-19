a20c4237fc6f5712b9b9532304eab1e3f1c01789
NeythonLecStreitz <nlecstreitz@oxy.edu>
2022-09-19 14:31:51 -0700

    Added function docstrings and cleaned up code

--- barbell-velocity-tracker.py
+++ barbell-velocity-tracker.py
@@ -5,8 +5,18 @@ import imutils
 import time
 from pyzbar import pyzbar
 import argparse
+import pandas as pd
 
 def read_qr(frame):
+	"""Detects a QR code within a given frame, draws border around QR code and returns decoded information
+
+	Args:
+		frame (_type_): A single frame from the inputted video.
+
+	Returns:
+		frame (_type_): _description_
+	"""
+	
 	barcodes = pyzbar.decode(frame)
 	for barcode in barcodes:
 		x, y, w, h = barcode.rect
@@ -17,14 +27,27 @@ def read_qr(frame):
 		
 		font = cv.FONT_HERSHEY_DUPLEX
 		cv.putText(frame, barcode_info, (x + 6, y - 6), font, 2.0, (255, 255, 255), 1)
-		
-		with open("barcode_result.txt", mode ='w') as file:
-			file.write("Recognized Barcode:" + barcode_info)
 	
 	return frame
 
+def frame_preprocess(frame):
+    
+    # Resize frame to 800/600
+	frame = imutils.resize(frame, width=800)
+ 
+	# Convert to grayscale
+	grayscale = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+ 
+	# Gaussian Blur
+	blurred = cv.GaussianBlur(grayscale, (11, 11), 0)
+ 
+	# Binary Threshold
+	ret, processed_frame = cv.threshold(blurred, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+ 
+	return processed_frame
+
 def main():
-    # construct the argument parse and parse the arguments on script call
+	# construct the argument parse and parse the arguments on script call
 	ap = argparse.ArgumentParser()
 	# For tracking ball from .mp4 video
 	ap.add_argument("-v", "--video",
@@ -39,8 +62,15 @@ def main():
 	else:
 		camera = cv.VideoCapture(args_dict["video"])
  
-	# allow the camera or video file to warm up
+	# allow the camera/video file to warm up
 	time.sleep(2.0)
+ 
+	# Create DataFrame to hold coordinates and time
+	data_columns = ['x', 'y', 'time']
+	data_df = pd.DataFrame(data = None, columns=data_columns, dtype=float)
+
+	# Read time at video start
+	start = time.time()
 	
 	while True:
 		(grabbed, frame) = camera.read()
@@ -48,14 +78,11 @@ def main():
 		if args_dict.get("video") and not grabbed:
 			break
 
-		# resize frame, blur it, and convert from BGR to HSV color space
-		frame = imutils.resize(frame, width=1000)
-		grayscale = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-		blurred = cv.GaussianBlur(grayscale, (11, 11), 0)
-		ret, processed_frame = cv.threshold(blurred, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+		processed_frame = frame_preprocess(frame)
   		
 		frame = read_qr(processed_frame)
 		cv.imshow('Barcode/QR Code Reader', frame)
+   
 		if cv.waitKey(1) & 0xFF == 27:
 			break
 		
